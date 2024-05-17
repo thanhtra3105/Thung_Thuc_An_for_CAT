e7a49b6433c19b9b9821b57b21c45f9bb5fd82c9
ThanhBan04 <134537032+ThanhBan04@users.noreply.github.com>
2024-05-17 21:52:52 +0700

    Add files via upload
    Đổi báo hiệu bằng còi thành báo hiệu bằng

--- Ket_noi_LCD_va_CBSA_va_coi_version2.ino
+++ Ket_noi_LCD_va_CBSA_va_coi_version2.ino
@@ -1,4 +1,5 @@
 
+
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
@@ -7,6 +8,8 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 const int trig = D4;
 const int echo = D5;
 const int bz = D6;
+int led1 = D7;
+int led2 = D8;
 unsigned long startTime = 0;
 bool isSignaling;
 int distance;
@@ -17,30 +20,59 @@ void setup() {
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
   pinMode(bz, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
 }
 
 void loop() {
-  do_KC();                                         // do khoang cach = distance;
-  distance = constrain(distance, 0, 10);           // gioi han kc tu 0-10cm, thay doi gia tri cho phu hop nghe
-  int food_amount = map(distance, 0, 10, 100, 0);  // convert luong thuc an tu khoang cach theo ti le %, vd: kc la 10(max) la khong con thuc an -> luong_thuc_an = 0
+  distance = do_KC();                                         // do khoang cach = distance;
+  distance = constrain(distance, 0, 10);           // gioi han kc tu 0-10cm, thay doi gia tri cho phu hop 
+  int food_amount = map(distance, 2, 10, 100, 0);  // convert luong thuc an tu khoang cach theo ti le %, vd: kc la 10(max) la khong con thuc an -> luong_thuc_an = 0
 
   lcd.setCursor(2, 0);
   lcd.print("LUONG THUC AN");
-  lcd.setCursor(7, 1);
-  lcd.print("   ");
+  lcd.setCursor(14, 1);
+  lcd.print("%");
+  if(food_amount == 100){
+  lcd.setCursor(11, 1);
   lcd.print(food_amount);
+  }else if(food_amount >= 10){
+   lcd.setCursor(11, 1);
+   lcd.print(" ");
+   lcd.print(food_amount);
+  }else {
+    lcd.setCursor(11, 1);
+    lcd.print("  ");
+    lcd.print(food_amount);
+  }
+  Serial.println("Luong thuc an: ");
+  Serial.println(food_amount);
+  delay(500);
   int pre_time = millis();
-  if (food_amount == 0)  // het thuc an
+  if (food_amount == 1)  // het thuc an
   {
     // bao
-    while (millis() - pre_time < 2000)  // cho bao 2s
-    {
-      digitalWrite(bz, 1);
-      delay(100);
-      digitalWrite(bz, 0);
-      delay(100);
-      //lcd.clear();
-    }
+    // while (millis() - pre_time < 2000)  // cho bao 2s
+    // { Serial.println("Coi dang bao");
+    //   digitalWrite(bz, 1);
+    //   delay(100);
+    //   digitalWrite(bz, 0);
+    //   delay(100);
+    //   //lcd.clear();
+    // }
+    //Bao bang led nhap nhay:
+    Serial.println("Den bao hieu dang ON");
+    digitalWrite(led1, 1);
+    delay(1000);
+    digitalWrite(led1, 0);
+    delay(1000);
+    digitalWrite(led2, 1);
+    delay(1000);
+    digitalWrite(led2, 0);
+    delay(1000);
+  }else{
+   Serial.println("Den bao hieu dang OFF");
   }
 }
 
@@ -60,4 +92,3 @@ int do_KC() {
 
   return distance;
 }
-
