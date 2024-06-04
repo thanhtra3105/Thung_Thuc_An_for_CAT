17fc2aaad7bfe593baeccec9b333a9a667bca5f8
ThanhBan04 <134537032+ThanhBan04@users.noreply.github.com>
2024-06-04 15:10:47 +0700

    completed

---
+++ Do_muc_thuc_an.ino
@@ -0,0 +1,93 @@
+//Hien thi luong thuc an và canh bao
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int trig = 4;
+const int echo = 5;
+const int bz = 6;
+int led1 = 7;
+int led2 = 8;
+int distance;
+void setup() {
+  //set up lcd
+  lcd.init();
+  lcd.backlight();//bat den nen
+
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(bz, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+void loop(){
+  //Run hien thi luong thuc an
+  distance = do_KC();                                         // do khoang cach = distance;
+  distance = constrain(distance, 2, 12);           // gioi han kc tu 0-10cm, thay doi gia tri cho phu hop 
+  int food_amount = map(distance, 2, 12, 100, 0);  // convert luong thuc an tu khoang cach theo ti le %, vd: kc la 10(max) la khong con thuc an -> luong_thuc_an = 0
+
+  lcd.setCursor(2, 0);
+  lcd.print("LUONG THUC AN");
+  lcd.setCursor(14, 1);
+  lcd.print("%");
+  if(food_amount == 100){
+  lcd.setCursor(11, 1);
+  lcd.print(food_amount);
+  }else if(food_amount >= 10){
+   lcd.setCursor(11, 1);
+   lcd.print(" ");
+   lcd.print(food_amount);
+  }else {
+    lcd.setCursor(11, 1);
+    lcd.print("  ");
+    lcd.print(food_amount);
+  }
+  Serial.println("Luong thuc an: ");
+  Serial.println(food_amount);
+  delay(500);
+  //int pre_time = millis();
+  if (food_amount == 0)  // het thuc an
+  {
+    // bao
+    // while (millis() - pre_time < 2000)  // cho bao 2s
+    // { Serial.println("Coi dang bao");
+    //   digitalWrite(bz, 1);
+    //   delay(100);
+    //   digitalWrite(bz, 0);
+    //   delay(100);
+    //   //lcd.clear();
+    // }
+    //Bao bang led nhap nhay:
+    Serial.println("Den bao hieu dang ON");
+    digitalWrite(led1, 1);
+    delay(1000);
+    digitalWrite(led1, 0);
+    delay(1000);
+    digitalWrite(led2, 1);
+    delay(1000);
+    digitalWrite(led2, 0);
+    delay(1000);
+  }else{
+   Serial.println("Den bao hieu dang OFF");
+  }
+}
+
+int do_KC() {
+  unsigned long duration;
+  int distance;
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(3);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trig, LOW);
+  delayMicroseconds(3);
+
+  duration = pulseIn(echo, HIGH);
+  distance = duration / 2 / 29.412;
+
+  return distance;
+
+}
